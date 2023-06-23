7067ff8b99c17af19f6209ecb54118f64756a83b
shahrukh <mohdshahrukhmzn1@gmail.com>
2023-06-23 18:23:27 +0530

    changes updated

--- AssingmentObjectCreationAutowired/src/main/java/org/example/testApi/ControllerApi.java
+++ AssingmentObjectCreationAutowired/src/main/java/org/example/testApi/ControllerApi.java
@@ -1,5 +1,7 @@
 package org.example.testApi;
 
+import org.example.MailHandler;
+import org.example.Main;
 import org.example.module.Animal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerApi {
+
+    String []arg= {"1"} ;
     @Autowired
     Animal animal;
 
     @GetMapping("animal")
     public String getDataThroughApi(){
-        simple
+        System.out.println("Helllo call api ..............!!!!!!!!!!!!!!");
+        Main.main(arg);
         return animal.getName();
+
     }
 }
